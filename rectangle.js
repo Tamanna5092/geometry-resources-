1fdb7279011d091ba882bb4c37dc5e17bb3403e5
Tamanna5092 <tamannaaktermoni51@gmail.com>
2024-02-24 19:14:16 +0600

    add rectangle area

--- rectangle.js
+++ rectangle.js
@@ -1,3 +1,15 @@
 function rectangleCalculation(){
-    console.log('connect')
+    // get width value
+    const rectangleInputWidth = document.getElementById('rectangle-width');
+    const rectangleTextWidth = rectangleInputWidth.value;
+    const width = parseFloat(rectangleTextWidth);
+    console.log(width)
+    // get length value
+    const rectangleInputLength = document.getElementById('rectangle-length');
+    const rectangleTextLength = rectangleInputLength.value;
+    const length = parseFloat(rectangleTextLength);
+    console.log(length)
+    // get area value
+    const area = width * length;
+    console.log('area of the rectangle is',area)
 }
